039932a03b915a0c980ab058de90f3d2020fd891
Hamza Jerbi <hamza.jerbi@everis.com>
2019-02-16 12:24:23 +0100

    Updated model to struct

---
+++ musicNet/Model/MusicTrack.swift
@@ -0,0 +1,17 @@
+//
+//  musicModel.swift
+//  musicNet
+//
+//  Created by Hamza Jerbi on 16/02/2019.
+//  Copyright © 2019 Hamza Jerbi. All rights reserved.
+//
+
+import Foundation
+
+
+struct MusicTrack {
+    
+    let title: String
+    let description:  String
+    
+}
