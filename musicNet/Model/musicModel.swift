e8283504fc42f3d190fff52bb8b2553a51432ccb
Hamza Jerbi <hamza.jerbi@everis.com>
2019-02-16 11:24:20 +0100

    Created a simple model

---
+++ musicNet/Model/musicModel.swift
@@ -0,0 +1,22 @@
+//
+//  musicModel.swift
+//  musicNet
+//
+//  Created by Hamza Jerbi on 16/02/2019.
+//  Copyright © 2019 Hamza Jerbi. All rights reserved.
+//
+
+import Foundation
+
+
+class musicModel {
+    
+    var title: String
+    var description:  String
+    
+    init(title: String, description: String) {
+        
+        self.title = title
+        self.description = description
+    }
+}
